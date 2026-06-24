8424947d676197e71cee2f6d945d14d315f0fcab
Joaozeraah21 <lima.matias.joao@escola.pr.gov.br>
2026-06-24 17:54:41 -0300

    Update lista.py

--- lista.py
+++ lista.py
@@ -2,10 +2,14 @@ def cadastrar():
     nome = input("Digite o nome do aluno a ser cadastrar: ")
     return nome
 
+def mostrar(lista):
+    print("nome dos alunos: ")
+
 listaAlunos = ["Gustavo","Arthur","Daniel"]
-listaAlunos.append(cadastrar())
-print("Nome dos alunos da sala: ")
-print(listaAlunos[0])
-print(listaAlunos[1])
-print(listaAlunos[2])
-print(listaAlunos[3])
+opcao = 0
+while opcao != 10:
+    opcao = int(input("Digite 1 para mostrar os alunos cadastrados\nDigite 2 cadastrar\nDigite 3 para editar\nDigite 3 para editar\nDigite 4 para deletar"))
+    if opcao == 1:
+        mostrar(lidtaAlumos)
+    elif opcao == 2:
+        listaAlunos.append(cadastrar())
